121091af726341781d09dbe5cdc0499319caac98
abhishekdepro <abhishekde@hotmail.com>
2015-08-09 12:17:10 +0530

    Final edit

--- app.js
+++ app.js
@@ -10,14 +10,15 @@
 var mongojs = require('mongojs');
 var restify = require('restify');
 var ip_addr = '127.0.0.1';
-var port    =  process.env.PORT || 80;
+var port    =  process.env.PORT || 1337;
 
 var server = restify.createServer({
     name : "Ecosquare API"
 });
 
-server.listen(port ,ip_addr, function(){
-    console.log('%s listening at %s ', server.name , server.url);
+server.listen(port , function(){
+    //console.log('%s listening at %s ', server.name , server.url);
+    console.log('started');
 });
 
 server.use(restify.queryParser());
