e627915d0c90aa7b0a5f4846febf9ef84d2b02d8
abhishekdepro <abhishekde@hotmail.com>
2015-08-20 14:20:54 +0530

    Fixed layout

--- Android/Ecosquare/app/src/main/java/abhishekdey/ecosquare/MapsActivity.java
+++ Android/Ecosquare/app/src/main/java/abhishekdey/ecosquare/MapsActivity.java
@@ -3,7 +3,10 @@ package abhishekdey.ecosquare;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -11,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -23,6 +27,13 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Locale;
 
 
 public class MapsActivity extends ActionBarActivity implements GoogleMap.OnMarkerClickListener,LocationListener {
@@ -38,7 +49,7 @@ public class MapsActivity extends ActionBarActivity implements GoogleMap.OnMarke
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
+        MapsInitializer.initialize(getApplicationContext());
         setUpMapIfNeeded();
         locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
@@ -83,15 +94,150 @@ public class MapsActivity extends ActionBarActivity implements GoogleMap.OnMarke
         // Setting Current Latitude
         lat = location.getLatitude();
 
+        Geocoder geocoder;
+        List<Address> addresses=new List<Address>() {
+            @Override
+            public void add(int location, Address object) {
 
+            }
+
+            @Override
+            public boolean add(Address object) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int location, Collection<? extends Address> collection) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Address> collection) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public boolean contains(Object object) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> collection) {
+                return false;
+            }
+
+            @Override
+            public Address get(int location) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object object) {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public Iterator<Address> iterator() {
+                return null;
+            }
+
+            @Override
+            public int lastIndexOf(Object object) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<Address> listIterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public ListIterator<Address> listIterator(int location) {
+                return null;
+            }
+
+            @Override
+            public Address remove(int location) {
+                return null;
+            }
+
+            @Override
+            public boolean remove(Object object) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> collection) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> collection) {
+                return false;
+            }
+
+            @Override
+            public Address set(int location, Address object) {
+                return null;
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @NonNull
+            @Override
+            public List<Address> subList(int start, int end) {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @NonNull
+            @Override
+            public <T> T[] toArray(T[] array) {
+                return null;
+            }
+        };
+        geocoder = new Geocoder(this, Locale.getDefault());
+try {
+    addresses = geocoder.getFromLocation(lat, lon, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+    String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+    String city = addresses.get(0).getLocality();
+    String state = addresses.get(0).getAdminArea();
+    String country = addresses.get(0).getCountryName();
+    String postalCode = addresses.get(0).getPostalCode();
+    String knownName = addresses.get(0).getFeatureName();
+
+}catch (IOException ex){
+    Toast.makeText(getBaseContext(), "Error in parsing", Toast.LENGTH_SHORT).show();
+}
         myMarker = mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(lat, lon))
-                .title("My Spot").icon(BitmapDescriptorFactory.fromResource(R.drawable.start))
-                .snippet("This is my spot!"));
+                .title(addresses.get(0).getAddressLine(0) + ", " + addresses.get(0).getAddressLine(1)).icon(BitmapDescriptorFactory.fromResource(R.drawable.start))
+                .snippet(addresses.get(0).getLocality()));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 10.0f));
         myMarker.setDraggable(true);
         mMap.setOnMarkerClickListener(this);
-        //myMarker.showInfoWindow();
+        myMarker.showInfoWindow();
         //mMap.setInfoWindowAdapter(adapter);
         //adapter.getInfoWindow(myMarker);
     }
@@ -187,6 +333,10 @@ public class MapsActivity extends ActionBarActivity implements GoogleMap.OnMarke
         else if (marker.equals(y))
         {
             y.showInfoWindow();
+        }
+        else if (marker.equals(myMarker))
+        {
+            myMarker.showInfoWindow();
         }
         return true;
     }
