a901133f8848e919726ede3a4c6e4949e8535c0d
abhishekdepro <abhishekde@hotmail.com>
2015-08-28 13:05:14 +0530

    Added progress dialog

--- Android/Ecosquare/app/src/main/java/abhishekdey/ecosquare/MapsActivity.java
+++ Android/Ecosquare/app/src/main/java/abhishekdey/ecosquare/MapsActivity.java
@@ -2,9 +2,11 @@ package abhishekdey.ecosquare;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
@@ -30,6 +32,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
@@ -57,6 +61,7 @@ import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -76,12 +81,12 @@ import java.util.Map;
 
 public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarkerClickListener,LocationListener {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    public GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Marker myMarker,x,y;
     private double lat,lon;
     LocationManager locationManager ;
     String provider;
-
+    ProgressDialog progress;
 
     GoogleMap.InfoWindowAdapter adapter;
     private String activeMarker = "myMarker";
@@ -103,31 +108,50 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
             @Override
             public void onClick(View v) {
 
-                //Trying Ion
-                JsonObject json = new JsonObject();
-                json.addProperty("u_id", "8981169454");
-                json.addProperty("emp_id", "876567890");
-                json.addProperty("lat", Double.toString(lat));
-                json.addProperty("lon", Double.toString(lon));
-                json.addProperty("paper", "5");
-                json.addProperty("plastic", "10");
-                json.addProperty("mode", "cash");
-                json.addProperty("status", "init");
+                progress = ProgressDialog.show(MapsActivity.this, "Working",
+                        "The minions are working..", true);
 
-                Ion.with(getApplicationContext())
-                        .load("http://ecosquare.herokuapp.com/transaction")
-                        .setJsonObjectBody(json)
-                        .asJsonObject()
-                        .setCallback(new FutureCallback<JsonObject>() {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        //Trying Ion
+                        JsonObject json = new JsonObject();
+                        json.addProperty("u_id", "8981169454");
+                        json.addProperty("emp_id", "876567890");
+                        json.addProperty("lat", Double.toString(lat));
+                        json.addProperty("lon", Double.toString(lon));
+                        json.addProperty("paper", "5");
+                        json.addProperty("plastic", "10");
+                        json.addProperty("mode", "cash");
+                        json.addProperty("status", "init");
+
+                        Ion.with(getApplicationContext())
+                                .load("http://ecosquare.herokuapp.com/transaction")
+                                .setJsonObjectBody(json)
+                                .asJsonObject()
+                                .setCallback(new FutureCallback<JsonObject>() {
+                                    @Override
+                                    public void onCompleted(Exception e, JsonObject result) {
+                                        if(e!=null){
+                                            Toast.makeText(getBaseContext(), "Data : "+e.getStackTrace(), Toast.LENGTH_LONG).show();
+                                        }else{
+                                            Toast.makeText(getBaseContext(), "Pickup added successfully! We will contact you soon.", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+
+                        runOnUiThread(new Runnable() {
                             @Override
-                            public void onCompleted(Exception e, JsonObject result) {
-                                if(e!=null){
-                                    Toast.makeText(getBaseContext(), "Data : "+e.getStackTrace(), Toast.LENGTH_LONG).show();
-                                }else{
-                                    Toast.makeText(getBaseContext(), "Pickup added successfully! We will contact you soon.", Toast.LENGTH_LONG).show();
-                                }
+                            public void run()
+                            {
+                                progress.dismiss();
                             }
                         });
+                    }
+                }).start();
+
+
 
 
                 /*try {
@@ -347,7 +371,7 @@ try {
                 .position(new LatLng(lat, lon))
                 .title(addresses.get(0).getAddressLine(0) + ", " + addresses.get(0).getAddressLine(1)).icon(BitmapDescriptorFactory.fromResource(R.drawable.pin))
                 .snippet(addresses.get(0).getLocality()));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 10.0f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 11.0f));
         myMarker.setDraggable(true);
         mMap.setOnMarkerClickListener(this);
         myMarker.showInfoWindow();
@@ -416,6 +440,7 @@ try {
     }
 
 
+
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
@@ -448,19 +473,18 @@ try {
             @Override
             public void onMarkerDragEnd(Marker marker) {
                 if (mMap.getMyLocation() != null) {
-                     lat = marker.getPosition().latitude;
-                     lon = marker.getPosition().longitude;
+                    lat = marker.getPosition().latitude;
+                    lon = marker.getPosition().longitude;
                     Location location = new Location("Test");
                     location.setLatitude(lat);
                     location.setLongitude(lon);
-                   onLocationChanged(location);
+                    onLocationChanged(location);
                 }
-                if(marker.equals(x)){
+                if (marker.equals(x)) {
                     activeMarker = "x";
-                }else if(marker.equals(y)){
+                } else if (marker.equals(y)) {
                     activeMarker = "y";
-                }
-                else{
+                } else {
                     activeMarker = "myMarker";
                 }
 
@@ -473,6 +497,7 @@ try {
     public void locate_me(View v)
     {
         onLocationChanged(mMap.getMyLocation());
+        call();
     }
 
     @Override
@@ -481,6 +506,8 @@ try {
         if (marker.equals(x))
         {
             x.showInfoWindow();
+
+
         }
         else if (marker.equals(y))
         {
@@ -489,7 +516,115 @@ try {
         else if (marker.equals(myMarker))
         {
             myMarker.showInfoWindow();
+
         }
         return true;
     }
+
+    public void call(){
+    }
+
+    public void drawPath(String  result) {
+
+        try {
+            //Tranform the string into a json object
+            final JSONObject json = new JSONObject(result);
+            JSONArray routeArray = json.getJSONArray("routes");
+            JSONObject routes = routeArray.getJSONObject(0);
+            JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+            String encodedString = overviewPolylines.getString("points");
+            List<LatLng> list = decodePoly(encodedString);
+
+            for(int z = 0; z<list.size()-1;z++){
+                LatLng src= list.get(z);
+                LatLng dest= list.get(z+1);
+                Polyline line = mMap.addPolyline(new PolylineOptions()
+                        .add(new LatLng(src.latitude, src.longitude), new LatLng(dest.latitude,   dest.longitude))
+                        .width(2)
+                        .color(Color.BLUE).geodesic(true));
+            }
+
+        }
+        catch (JSONException e) {
+            Log.e("Json_error",e.getStackTrace().toString());
+        }
+    }
+
+    public String makeURL (double sourcelat, double sourcelog, double destlat, double destlog ){
+        StringBuilder urlString = new StringBuilder();
+        urlString.append("http://maps.googleapis.com/maps/api/directions/json");
+        urlString.append("?origin=");// from
+        urlString.append(Double.toString(sourcelat));
+        urlString.append(",");
+        urlString
+                .append(Double.toString( sourcelog));
+        urlString.append("&destination=");// to
+        urlString
+                .append(Double.toString( destlat));
+        urlString.append(",");
+        urlString.append(Double.toString( destlog));
+        urlString.append("&sensor=false&mode=driving&alternatives=true");
+        return urlString.toString();
+    }
+
+    private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng( (((double) lat / 1E5)),
+                    (((double) lng / 1E5) ));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
+    private class connectAsyncTask extends AsyncTask<Void, Void, String>{
+        private ProgressDialog progressDialog;
+        String url;
+        connectAsyncTask(String urlPass){
+            url = urlPass;
+        }
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            JSONParser jParser = new JSONParser();
+            String json = jParser.getJSONFromUrl(url);
+            return json;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if(result!=null){
+                drawPath(result);
+            }
+        }
+    }
+
 }
